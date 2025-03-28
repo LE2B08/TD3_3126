7e1298da0b4fab23a306df8eb1582072148e075f
wataruakimoto <k023g0116@g.neec.ac.jp>
2025-03-28 16:18:47 +0900

    akimotoSceneでだがDynamicCameraの基礎部分を実装した。GameSceneの準備ができ次第書き換えられるようにしてある。ついでにFieldの仮リソースも入れておいた。

--- EngineLayers/ApplicationLayer/SceneManagement/LogicLayer/AkimotoScene.cpp
+++ EngineLayers/ApplicationLayer/SceneManagement/LogicLayer/AkimotoScene.cpp
@@ -9,9 +9,11 @@
 #include "Wireframe.h"
 #include <SkyBoxManager.h>
 #include <SpriteManager.h>
+#include "FadeManager.h"
 
 void AkimotoScene::Initialize()
 {
+	FadeManager::GetInstance()->StartFadeOut();
 	dxCommon_ = DirectXCommon::GetInstance();
 	textureManager = TextureManager::GetInstance();
 	input_ = Input::GetInstance();
@@ -19,6 +21,8 @@ void AkimotoScene::Initialize()
 
 	player_ = std::make_unique<Player>();
 	player_->Initialize();
+	player_->SetPosition({ 20.0f, 0.0f, -20.0f });
+	player_->SetIsGameStart(true);
 
 	enemy_ = std::make_unique<Enemy>();
 	enemy_->Initialize();
@@ -26,6 +30,15 @@ void AkimotoScene::Initialize()
 
 	field_ = std::make_unique<Field>();
 	field_->Initialize();
+
+	camera_ = Object3DCommon::GetInstance()->GetDefaultCamera();
+
+	dynamicCamera_ = std::make_unique<DynamicCamera>();
+	dynamicCamera_->Initialize();
+	dynamicCamera_->SetPlayer(player_.get());
+	dynamicCamera_->SetEnemy(enemy_.get());
+
+	Object3DCommon::GetInstance()->SetDefaultCamera(camera_);
 }
 
 void AkimotoScene::Update()
@@ -57,13 +70,34 @@ void AkimotoScene::Update()
 		}
 	}
 
+	// フィールドの更新
+	field_->Update();
+
+	player_->SetMinMoveLimit(field_->GetMinPosition());
+	player_->SetMaxMoveLimit(field_->GetMaxPosition());
+
+	// プレイヤーの更新
+	player_->Update();
+
+	enemy_->SetMinMoveLimit(field_->GetMinPosition());
+	enemy_->SetMaxMoveLimit(field_->GetMaxPosition());
+
+	// 敵の更新
 	enemy_->Update();
 
-	field_->Update();
+	// ダイナミックカメラの更新
+	dynamicCamera_->Update();
+
+	// 計算したあとのカメラの値をセット
+	camera_->SetScale(dynamicCamera_->GetScale());
+	camera_->SetRotate(dynamicCamera_->GetRotate());
+	camera_->SetTranslate(dynamicCamera_->GetTranslate());
+
+	// カメラの更新
+	camera_->Update();
 }
 
-void AkimotoScene::Draw()
-{
+void AkimotoScene::Draw() {
 	/// ------------------------------------------ ///
 	/// ---------- スカイボックスの描画 ---------- ///
 	/// ------------------------------------------ ///
@@ -85,9 +119,20 @@ void AkimotoScene::Draw()
 	// オブジェクト3D共通描画設定
 	Object3DCommon::GetInstance()->SetRenderSetting();
 
+	// プレイヤーの描画
+	player_->Draw();
+
+	// 敵の描画
 	enemy_->Draw();
 
+	// フィールドの描画
 	field_->Draw();
+
+	// ラインの描画
+	Wireframe::GetInstance()->DrawLine(player_->GetPosition(), enemy_->GetPosition(), { 1.0f, 0.0f, 0.0f, 1.0f });
+
+	// 中心点の描画
+	Wireframe::GetInstance()->DrawCircle((player_->GetPosition() + enemy_->GetPosition()) / 2.0f, 0.19f, 16, { 1.0f, 0.0f, 0.0f, 1.0f });
 }
 
 void AkimotoScene::Finalize()
@@ -96,7 +141,11 @@ void AkimotoScene::Finalize()
 
 void AkimotoScene::DrawImGui()
 {
+	player_->DrawImGui();
+
 	enemy_->ShowImGui("Enemy");
 
 	field_->ShowImGui("Field");
+
+	dynamicCamera_->ShowImGui("DynamicCamera");
 }
